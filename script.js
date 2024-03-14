619a015be93d8a4a9a82a5a6fad3d2ca1bf820e5
mike <mike@hk>
2024-03-14 18:51:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+////////////*студент,адрес,номер телефона*/////////
+
+let name="Студент.";
+let address="Адрес:дудаева,";
+let phone="Номер телефона:89647689900";
+let c=name+address+phone;
+console.log(c)
+
+
+
+
+
+
+const namee=prompt("Студент");
+const addresss=prompt("Адрес:");
+const phonee=prompt("Номер телефона:");
+console.log(namee,addresss,phonee);
+
+
+
+//////*число*//////
+let age = 12;
+let width = 32;
+
+
+
+/////////*строки*/////////
+let ac = "apple";
+let bc = 'hi';
+
+
+//////*булевы значения*///////
+let isstudent=true;
+let teacher=false;
+
+
+////////*underfined and null*/////
+
+let myvar=null;
+let myvar2;
+
+
+
+///////*object*////////
+let person={
+    age:30
+}
+
+
+/////////*arrays*///////
+let fruits=["apple","bananas"]
+
+
+
+/////////*symbol*//////////
+let id= Symbol("id")
+
+
+
+
+///////////*bigint*/////////////
+let bigint=32429445239470297n
+
+
+
+let a=20;
+let b=23;
+b=13;
+let d =a+b;
+console.log(d)
